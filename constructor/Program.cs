3160204b654f0a69f6c976c5e36392b9e9b9fd2d
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2024-08-17 12:12:47 +0530

    Add positions file

---
+++ constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s1 = new student(501, "anr");
+            s1.showstudent();
+            student s2 = new student(502, "mouni", "cse");
+            s2.showstudent();
+            student s3 = new student(503, "basava", "cse", 4000.00);
+            s3.showstudent();
+
+
+        }
+    }
+}
---
+++ constructor/student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor
+{
+    internal class student
+    {
+        int studentid;
+        string studentName;
+        String studentbranch;
+        double studentfee;
+        public student()
+        {
+
+        }
+        public student(int sid,string sName)
+        {
+            studentid = sid;
+            studentName = sName;
+        }
+        public student(int sid, string sName,string sbr)
+        {
+            studentid = sid;
+            studentName = sName;
+            studentbranch = sbr;
+        }
+        public student(int sid, string sName, string sbr,double sfee)
+        {
+            studentid = sid;
+            studentName = sName;
+            studentbranch = sbr;
+            studentfee = sfee;
+        }
+        public void showstudent()
+        {
+            Console.WriteLine(studentid + "" + studentName + ""
+                + studentbranch + "" +studentfee);
+        }
+        ~student()
+        {
+            Console.WriteLine("destructor");
+        }
+    } 
+    
+    
+}
