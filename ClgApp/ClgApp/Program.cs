80e6361d9446a442c24629d14cbc5f31ee554411
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2024-11-17 11:22:38 +0530

    database server

---
+++ ClgApp/ClgApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClgApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            studentContext context = new studentContext();
+            // Read products - using foreach
+
+            foreach (Student s in context.Students)
+            {
+                Console.WriteLine(s.Id + " " + s.Name + " " + s.Branch + " " + s.College + " " + s.City );
+            }
+            Student e = new Student();
+
+            Console.WriteLine("Enter Student Id:");
+            e.Id= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the student name:");
+            e.Name=Console.ReadLine();
+
+            Console.WriteLine("Enter the College:");
+            e.Branch= Console.ReadLine();
+
+            Console.WriteLine("Enter the City:");
+            e.City= Console.ReadLine();
+
+            Console.WriteLine("Enter the state");
+            e.state= Console.ReadLine();
+
+            context.Students.Remove(e);
+            context.SaveChanges();
+            Console.WriteLine("new add is created successfully");
+        }
+    }
+}
---
+++ ClgApp/ClgApp/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+
+namespace ClgApp
+{
+    internal class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        [Required]
+
+        public string Name { get; set; }
+        [Required]
+
+        public string Branch { get; set; }
+        [Required]
+
+        public string College {  get; set; }
+        [Required]
+
+        public string City {  get; set; }
+        [Required]
+
+        public string state {  get; set; }
+    }
+}
