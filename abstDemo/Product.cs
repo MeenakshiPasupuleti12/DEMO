a7a4719c3f4110a32fb19b6fa2a9d924754c7ae9
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2024-12-04 16:03:11 +0530

    essageAbstract

---
+++ abstDemo/Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstDemo
+{
+    partial class Product
+    {
+        partial void AddProduct();
+        partial void EditProduct();
+        partial void FindProduct();
+        partial void DeleteProduct();
+
+    }
+        //Developer 1
+        public partial class product
+        {
+            partial void AddProduct()
+            {
+                Console.WriteLine("Add product by developer1");
+            }
+            public void saveproduct()
+            {
+               AddProduct();
+            }
+        }
+        //Developer 2 
+        public partial class product
+        {
+            partial void EditProduct()
+            {
+
+            }
+        public void upadateproduct()
+        {
+
+        }
+       }
+        //Developer 3
+        public partial class product
+        {
+            partial void DeleteProduct()
+            {
+
+            }
+        }
+        //Developer 4
+        public partial class product
+        {
+            partial void FindProduct()
+            {
+
+            }
+        }
+
+
+    
+}
