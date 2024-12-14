4170dff76b67ddea96a287cfc7ef99be8b0410d0
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2024-12-14 18:53:18 +0530

    essageObjects in java Script

---
+++ object.js
@@ -0,0 +1,20 @@
+
+const student = {
+    name : "Meenakshi",
+    class : "B.TEC",
+    phone : 6797554433,
+}
+
+
+delete student.phone
+student.college="Riet";
+student.subjects=['telugu','English','Maths','physics']
+console.log(student.subjects[0])
+student.feedback="Riet is not right choice"
+student.year = 2020
+console.log(student.name)
+console.log(student)
+
+
+
+
