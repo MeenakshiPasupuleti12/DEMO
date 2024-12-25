9e964f7fca1914fd511313c7d94d4238b4ae1bc9
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2024-12-25 12:07:26 +0530

    essageEntity Frame Work

---
+++ Pinky/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            product p1 = new product(120,"mango","3kg",150.00);
+            p1.showproduct();
+            product p2 = new product(201, "grapee", "3kg", 100.00);
+            p2.showproduct();
+            product p3 = new product(202, "apple", "3kg", 150.00);
+            p3.showproduct();
+        }
+    }
+}
---
+++ Pinky/product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinky
+{
+    internal class product
+    {
+        int productId;
+        string prouductName;
+        string Quantity;
+        double price;
+        public product(int productId,string productName,string Quantity,double price)
+        {
+            this.productId = productId;
+            this.prouductName = productName;
+            this.Quantity = Quantity;
+            this.price = price;
+
+        }
+        public void showproduct()
+        {
+            Console.WriteLine(productId + " " + prouductName + " " + Quantity + " " + price);
+        }
+    }
+}
