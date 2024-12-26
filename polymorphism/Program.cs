3af9de19f9c0e220a2ed3a85b4660ccb137bebaa
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2024-12-26 22:15:00 +0530

    essagePolymorphism file

---
+++ polymorphism/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            mylib.Add(19, 29);
+            mylib.Add(19, 20, 30);
+            mylib.Add(".net", "c#");
+            mylib.Add(12, "meena");
+            mylib.Add(11.0, 178.0);
+            mylib.Add(140.00f, 23.09f);
+            mylib.Add(7396018872l, 2);
+            mylib.Add(12.0f, 132.099);
+        }
+    }
+}
---
+++ polymorphism/mylib.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    internal class mylib
+    {
+
+        public static void Add(int x, int y)
+        {
+            Console.WriteLine(" int int " + (x + y));
+        }
+        public static void Add(int x,int y,int z)
+        {
+            Console.WriteLine("int int int" + (x + y + z));
+        }
+        public static void Add(string x, string y)
+        {
+            Console.WriteLine("string string" + (x + y));
+        }
+        public static void Add(int x, string y)
+        {
+            Console.WriteLine("int string" + (x + y));
+        }
+        public static void Add(double x,double y)
+        {
+            Console.WriteLine("double double" + (x + y));
+        }
+        public static void Add(float x, float y)
+        {
+            Console.WriteLine("float float" + (x + y));
+        }
+        public static void Add( long x,int y)
+        {
+            Console.WriteLine("long int" + (x + y));
+        }
+        public static void  Add(float x ,double y)
+        {
+            Console.WriteLine(" float  double" + (x + y));
+        }
+
+    }
+
+}
