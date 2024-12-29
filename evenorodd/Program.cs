e962021283868f056522dbc33c15acfeb4040fd5
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2024-12-29 20:10:25 +0530

    essageTo check even odd numbers

---
+++ evenorodd/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenorodd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 1)
+            {
+                Console.WriteLine(number + "Is a postive number");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine(number + "neutral number");
+            }
+            else
+            {
+                Console.WriteLine(number + "is negative number");
+            }
+
+        }
+    }
+}
