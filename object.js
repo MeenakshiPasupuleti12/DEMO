95cfd068da131a04219c0e14ae988d1149d05208
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2025-01-21 18:39:09 +0530

    Update object.js

--- object.js
+++ object.js
@@ -11,7 +11,7 @@ delete student.phone
 student.college="Riet";
 student.subjects=['telugu','English','Maths','physics']
 console.log(student.subjects[0])
-student.feedback="Riet is not right choice"
+student.feedback="Riet is a right choice"
 student.year = 2020
 console.log(student.name)
 console.log(student)
