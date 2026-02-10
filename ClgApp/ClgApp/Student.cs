c40e55061e5dd3427201d0079dc37abdf532da96
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2026-02-10 08:14:41 +0530

    Fix import order for Student.cs

--- ClgApp/ClgApp/Student.cs
+++ ClgApp/ClgApp/Student.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-/using System.Threading.Tasks;
+using System.Threading.Tasks;
 
 namespace ClgApp
 {
