dc2a66392f60e321ce098e82d876aebd59f5121a
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2026-02-19 21:07:52 +0530

    Update Program.cs

--- ClgApp/ClgApp/Program.cs
+++ ClgApp/ClgApp/Program.cs
@@ -35,7 +35,7 @@ namespace ClgApp
             e.state= Console.ReadLine();
 
             context.Students.Remove(e);
-            //context.SaveChanges();
+            //ontext.SaveChanges();
             Console.WriteLine("new add is created successfully");
         }
     }
